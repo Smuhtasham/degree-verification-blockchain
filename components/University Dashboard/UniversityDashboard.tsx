27790302175da487fcb769500e727bb6a8c2ff13
Faizan Shakeel <103070823+ranafaizan937@users.noreply.github.com>
2025-05-27 20:40:37 +0500

    qr code

--- components/University Dashboard/UniversityDashboard.tsx
+++ components/University Dashboard/UniversityDashboard.tsx
@@ -306,7 +306,7 @@ const UniversityDashboard = () => {
       <div style={{ display: "none" }}>
         <div ref={qrCodeRef}>
           <QRCode
-            value={`https://degree-verification-frontend.vercel.app/degree-verification/${formData.cnic}`}
+            value={`https://www.vericred.online/degree-verification/${formData.cnic}`}
             size={100}
             bgColor="transparent"
             fgColor="#000000"
